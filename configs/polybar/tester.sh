0e6f1e4c2471d14a6b994e7eec3deb85755870c9
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-02-25 01:43:55 -0400

    Rice in i3wm

--- configs/polybar/tester.sh
+++ configs/polybar/tester.sh
@@ -9,4 +9,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+polybar top -c ~/.config/polybar/tester.ini &
+polybar mid -c ~/.config/polybar/tester.ini &
