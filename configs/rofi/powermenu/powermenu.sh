2f918d5e635e76e9b9a97ff75675f6bdb6dd28b1
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-02-26 02:29:15 -0400

    Fix Rofi Powermenu

--- configs/rofi/powermenu/powermenu.sh
+++ configs/rofi/powermenu/powermenu.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-# Available Styles
-# >> Created and tested on : rofi 1.6.0-1
-#
-# column_circle     column_square     column_rounded     column_alt
-# card_circle     card_square     card_rounded     card_alt
-# dock_circle     dock_square     dock_rounded     dock_alt
-# drop_circle     drop_square     drop_rounded     drop_alt
-# full_circle     full_square     full_rounded     full_alt
-# row_circle      row_square      row_rounded      row_alt
-
 dir="$HOME/.config/rofi/powermenu"
-
-uptime=$(uptime -p | sed -e 's/up //g')
-
 rofi_command="rofi -theme $dir/full_square.rasi"
 
 # Options
