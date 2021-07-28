2468ea88df66b6dff71cd43c4fc5422755c91baf
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-07-28 00:13:50 -0400

    Add: Add my config vim as a submodule

--- nvim/templates/files/c++/header.h
+++
@@ -1,6 +0,0 @@
-#ifndef %{'__' . substitute(toupper(expand('%:t')), '\(\.\|-\)', '_', 0) . '__'}
-#define %{0}
-
-%{CURSOR}
-
-#endif /* %{0} */
--- nvim/templates/files/c-sharp/interface.cs
+++
@@ -1,5 +0,0 @@
-using System;
-
-public interface #FILENAME# {
-
-}
