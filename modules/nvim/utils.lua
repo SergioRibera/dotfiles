8789246c05889b6f1a46aeed190afcbf8bee6683
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-04-27 00:37:52 -0400

    refactor(neovim): remove unnecesary util functions in lua

--- modules/nvim/utils.lua
+++ modules/nvim/utils.lua
@@ -1,21 +1,5 @@
 local utils = {}
 
-function utils.isModuleAvailable(name)
-    if package.loaded[name] then
-        return true
-    else
-        --- @diagnostic disable-next-line: deprecated
-        for _, searcher in ipairs(package.searchers or package.loaders) do
-            local loader = searcher(name)
-            if type(loader) == 'function' then
-                package.preload[name] = loader
-                return true
-            end
-        end
-        return false
-    end
-end
-
 function utils.dump_table(o)
     if type(o) == 'table' then
         local s = '{ '
@@ -27,28 +11,6 @@ function utils.dump_table(o)
     else
         return tostring(o)
     end
-end
-
-utils.is_str_empty = function (s)
-    return s == nil or s == ''
-end
-
-utils.get_lines = function (str)
-    local t = {}
-    local function helper(line) table.insert(t, line) return "" end
-    helper((str:gsub("(.-)\r?\n", helper)))
-    return t
-end
-
-utils.split_str = function (str, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = {}
-    for s in string.gmatch(str, "([^" .. sep .. "]+)") do
-        table.insert(t, s)
-    end
-    return t
 end
 
 function utils.get_hex(opts)
