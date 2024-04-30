3b6fd7037031ca96a8afe28eb7d6ace20e74a157
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-04-29 23:44:55 -0400

    fix(pkgs): remove config phase

--- pkgs/plymouth-macstyle/default.nix
+++ pkgs/plymouth-macstyle/default.nix
@@ -13,11 +13,8 @@ pkgs.stdenv.mkDerivation
 
   buildInputs = with pkgs; [ git ];
 
-  configPhase = ''
-    mkdir -p $out/share/plymouth/themes/mac-style
-  '';
-
   installPhase = ''
+    mkdir -p $out/share/plymouth/themes/mac-style
     cp -r src/mac-style $out/share/plymouth/themes/
     chmod +x $out/share/plymouth/themes/mac-style/mac-style.plymouth
   '';
