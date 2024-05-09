912b839c68e6a954dbad8121d5cf69c922111516
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-09 11:23:37 -0400

    fix: hyprland xdg portals

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -18,12 +18,11 @@ in
       enable = true;
       xdgOpenUsePortal = true;
       config = {
-        # common.default = [ "gtk" ];
+        common.default = [ "*" ];
         hyprland.default = [ "hyprland" ];
       };
 
       extraPortals = [
-        # pkgs.xdg-desktop-portal-gtk
         pkgs.xdg-desktop-portal-hyprland
       ];
     };
