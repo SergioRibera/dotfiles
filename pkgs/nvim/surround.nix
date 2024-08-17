86f71d7a974fe29a9ecf491f8fd13daf2d6bdc1f
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-08-16 20:43:16 -0400

    feat(pkgs): update surround

--- pkgs/nvim/surround.nix
+++ pkgs/nvim/surround.nix
@@ -1,11 +1,11 @@
 { pkgs }: pkgs.vimUtils.buildVimPlugin {
   pname = "nvim-surround";
-  version = "2024-04-11";
+  version = "2024-06-02";
   src = pkgs.fetchFromGitHub {
     owner = "kylechui";
     repo = "nvim-surround";
-    rev = "f7bb9fc4d68ad319d94b1d98ed16f279811f5cc8";
-    sha256 = "sha256-zi6FtK//HlBhndEYmzUQQtHR4ix73eAxHyB2Z3kmQz8=";
+    rev = "ec2dc7671067e0086cdf29c2f5df2dd909d5f71f";
+    sha256 = "sha256-DCNfT//qMnzIu4V9or3Q39h4XzLz9P4twtHnQHV2rrQ=";
   };
   meta.homepage = "https://github.com/kylechui/nvim-surround";
 }
