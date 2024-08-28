ad692c147bafd4dbfa9f39b60b5349dba89a50d2
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-08-28 01:30:40 -0400

    feat(apps): add zed configurations

--- home/editors/zed/default.nix
+++ home/editors/zed/default.nix
@@ -1,3 +1,81 @@
-{ pkgs, config, lib, ... }: {
-  environment.systemPackages = with pkgs; (lib.optionals config.gui.enable) [ zed-editor ];
+{ config, pkgs, lib, ... }: let
+  lsp = import ./lsp.nix { inherit config pkgs lib; };
+  zedNodeFixScript = pkgs.writeShellScriptBin "zedNodeFixScript" ''
+    nodeVersion="node-v${pkgs.nodejs.version}-linux-x64"
+    zedNodePath="/home/${config.user.username}/zed/node/$nodeVersion"
+
+    rm -rf $zedNodePath
+    ln -sfn ${pkgs.nodejs} $zedNodePath
+  '';
+  jsonGenerator = lib.generators.toJSON {};
+in {
+  home-manager.users."${config.user.username}" = lib.mkIf config.gui.enable {
+    home.packages = with pkgs; (lib.optionals config.gui.enable) [
+      zedNodeFixScript
+      zed-editor
+      nixd
+      biome
+      nodejs
+      taplo-lsp
+      vue-language-server
+      typescript-language-server
+      vscode-langservers-extracted
+      dockerfile-language-server-nodejs
+    ];
+    xdg.configFile."zed/settings.json".text = jsonGenerator ({
+      # The extensions that Zed should automatically install on startup.
+      auto_install_extensions = {
+        astro = true;
+        biome = true;
+        env = true;
+        html = true;
+        liquid = true; # TODO: lsp
+        nix = true;
+        nu = true;
+        vue = true;
+        toml = true;
+        dockerfile = true;
+        "Vitesse Theme" = true;
+        discord-presence = true;
+      };
+      auto_update = false;
+      ui_font_size = 16;
+      buffer_font_size = 14;
+      format_on_save = "on";
+      scrollbar.show = "never";
+      autosave = "on_focus_change";
+      relative_line_numbers = true;
+      jupyter.enabled = false;
+      vim_mode = true;
+      formatter = {
+        external = {
+          command = "${pkgs.biome}/bin/biome";
+          arguments = ["format" "--write" "--stdin-file-path" "{buffer_path}"];
+        };
+      };
+      theme = {
+        mode = "system";
+        dark = "Vitesse Dark Soft";
+        light = "Vitesse Dark Soft";
+      };
+      toolbar = {
+        breadcrumbs = true;
+        quick_actions = false;
+      };
+      calls = {
+        mute_on_join = true;
+        share_on_join = false;
+      };
+      tabs = {
+        git_status = true;
+        file_icons = true;
+      };
+      inlay_hints = {
+        enabled = true;
+        show_type_hints = false;
+        show_parameter_hints = true;
+        show_other_hints = true;
+      };
+    } // (lib.removeAttrs lsp ["__functor"]));
+  };
 }
---
+++ home/editors/zed/lsp.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, lib }: let
+  lsp_internal = name: bin: let
+    pkg = pkgs.${name};
+  in {
+    ${name} = {
+      command = "${pkg}/bin/${bin}";
+      args = ["--stdio"];
+    };
+  };
+  lsp_pkg = name:
+    (lsp_internal name name)
+    // {
+      __functor = self: bin: lsp_internal name bin;
+    };
+in {
+  lsp = (lib.removeAttrs ({
+    rust-analyzer = {
+      binary.path_lookup = true;
+      initialization_options = {
+        cachePriming = false;
+        check.features = "all";
+        cargo = {
+          features = "all";
+          allTargets = false;
+          buildScripts = true;
+        };
+        diagnostics = {
+          experimental = true;
+        };
+        imports = {
+          prefix = "self";
+          granularity = { group = "module"; };
+        };
+      };
+    };
+    biome = {
+      # settings = {
+      #   config_path = "/home/${config.user.username}/biome/config.json";
+      # };
+      binary = {
+        command = "${pkgs.biome}/bin/biome";
+        args = ["lsp"];
+      };
+    };
+    nu = {
+      command = "${pkgs.nushell}/bin/nu";
+      args = ["--lsp"];
+    };
+  }
+    // (lsp_pkg "nixd")
+    // (lsp_pkg "vue-language-server")
+    // (lsp_pkg "typescript-language-server")
+    // (lsp_pkg "astro-language-server" "astro-ls")
+    // (lsp_pkg "dockerfile-language-server-nodejs" "docker-langserver")
+    // (lsp_pkg "vscode-langservers-extracted" "vscode-json-language-server")
+    // (lsp_pkg "vscode-langservers-extracted" "vscode-css-language-server")
+    // (lsp_pkg "vscode-langservers-extracted" "vscode-html-language-server")
+    // (lsp_pkg "vscode-langservers-extracted" "vscode-markdown-language-server")
+    // (lsp_pkg "vscode-langservers-extracted" "vscode-eslint-language-server")
+  )
+  ["__functor"]);
+}
