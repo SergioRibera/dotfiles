e51cf02b9eec6d5bb7b5fd9017aa341ce420525e
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-05-10 18:49:08 -0400

    feat(gui): use gdm instead sddm

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -54,23 +54,22 @@ in {
       xkb.layout = "us";
       xkb.variant = "altgr-intl";
       videoDrivers = lib.optionals config.gui.enable [ "amdgpu" ];
-    };
 
     displayManager = {
-      sessionPackages = [
-        pkgs.niri
-        pkgs.sway
-      ];
-      sddm = lib.mkIf (pkgs.stdenv.buildPlatform.isLinux && config.gui.enable) {
-        enable = true;
-        theme = "catppuccin-mocha";
-        wayland.enable = true;
-        package = pkgs.kdePackages.sddm;
-        settings = {
-          Theme.CursorTheme = config.gui.cursor.name;
-          Wayland.CursorTheme = config.gui.cursor.name;
-        };
-      };
+      gdm.enable = config.gui.enable;
+      sessionPackages = builtins.map (o: pkgs."${o}") config.wm.actives;
+      # sessionPackages = [ pkgs.niri pkgs.sway ];
+      # sddm = lib.mkIf (pkgs.stdenv.buildPlatform.isLinux && config.gui.enable) {
+      #   enable = true;
+      #   theme = "catppuccin-mocha";
+      #   wayland.enable = true;
+      #   package = pkgs.kdePackages.sddm;
+      #   settings = {
+      #     Theme.CursorTheme = config.gui.cursor.name;
+      #     Wayland.CursorTheme = config.gui.cursor.name;
+      #   };
+      # };
     };
+};
   };
 }
