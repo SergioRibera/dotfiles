b16f5bcf37f7fa3efa163358ca6e0a6d43281fb6
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-05-19 18:19:01 -0400

    feat(host): add rnnoise to pipewire inputs
    Add noise reduce to pipewire input

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -44,10 +44,37 @@ in {
     pipewire = lib.mkIf config.audio {
       enable = true;
       alsa.enable = true;
-      alsa.support32Bit = true;
-      jack.enable = true;
       pulse.enable = true;
       wireplumber.enable = true;
+      extraConfig.pipewire = {
+        "99-input-denoising" = {
+          "context.modules" = [ {
+            "name" = "libpipewire-module-filter-chain";
+            "args" = {
+                "node.description" = "Noise Canceling source";
+                "media.name"       = "Noise Canceling source";
+                "filter.graph" = {
+                  "nodes" = [ {
+                    "type"   = "ladspa";
+                    "name"   = "rnnoise";
+                    "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    "label"  = "noise_suppressor_stereo";
+                    "control" = { "VAD Threshold (%)" = 50.0; };
+                  } ];
+                };
+                "audio.position" = [ "FL" "FR" ];
+                "capture.props" = {
+                  "node.name" = "effect_input.rnnoise";
+                  "node.passive" = true;
+                };
+                "playback.props" = {
+                  "node.name" = "effect_output.rnnoise";
+                  "media.class" = "Audio/Source";
+                };
+            };
+          }];
+        };
+      };
     };
 
     xserver = lib.mkIf pkgs.stdenv.buildPlatform.isLinux {
