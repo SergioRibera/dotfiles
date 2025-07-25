735e06d79b961c332e6e5dd577522358326ea1c3
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-07-25 15:15:06 -0400

    feat(host): setting display manager y xserver

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -77,26 +77,18 @@ in {
       };
     };
 
-    xserver = lib.mkIf pkgs.stdenv.buildPlatform.isLinux {
-      xkb.layout = "us";
-      xkb.variant = "altgr-intl";
-      videoDrivers = lib.optionals config.gui.enable [ "amdgpu" ];
-
     displayManager = {
       gdm.enable = config.gui.enable;
       sessionPackages = builtins.map (o: pkgs."${o}") config.wm.actives;
-      # sessionPackages = [ pkgs.niri pkgs.sway ];
-      # sddm = lib.mkIf (pkgs.stdenv.buildPlatform.isLinux && config.gui.enable) {
-      #   enable = true;
-      #   theme = "catppuccin-mocha";
-      #   wayland.enable = true;
-      #   package = pkgs.kdePackages.sddm;
-      #   settings = {
-      #     Theme.CursorTheme = config.gui.cursor.name;
-      #     Wayland.CursorTheme = config.gui.cursor.name;
-      #   };
-      # };
+      autoLogin = {
+        enable = !config.gui.enable;
+        user = config.user.username;
+      };
     };
-};
+
+    xserver = lib.mkIf pkgs.stdenv.buildPlatform.isLinux {
+      xkb.layout = "us";
+      xkb.variant = "altgr-intl";
+    };
   };
 }
