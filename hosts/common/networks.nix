9783960e07b52bfd8a56351ec968bd3a636a7aa5
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-12-22 22:49:50 -0400

    feat: enable hmac for openssh

--- hosts/common/networks.nix
+++ hosts/common/networks.nix
@@ -8,4 +8,10 @@
       };
     };
   };
+  services.openssh.settings.Macs = [
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+    "hmac-sha2-256"
+  ];
 }
