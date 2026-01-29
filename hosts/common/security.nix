12cc053c2453e078f5bb94146d0c84e658bc1c75
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-29 16:45:39 -0400

    feat: enalbe sudo-rs

--- hosts/common/security.nix
+++ hosts/common/security.nix
@@ -1,9 +1,44 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   inherit (config) gui;
+  inherit (lib) getExe';
 in
 {
   security.polkit.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
   security.rtkit.enable = pkgs.stdenv.buildPlatform.isLinux && gui.enable;
+
+  security.sudo.enable = false;
+  security.sudo-rs = {
+    enable = true;
+    extraConfig = ''
+      Defaults pwfeedback
+      Defaults env_keep += "EDITOR PATH"
+    '';
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+
+        commands = [
+          {
+            command = getExe' pkgs.systemd "systemctl";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = getExe' pkgs.systemd "reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = getExe' pkgs.systemd "shutdown";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+  };
 }
